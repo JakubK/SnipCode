304d1fad2630208bfef4740f6ceb2961218b9caa
Jakub Kuniszewski <jakubkuniszewski1@gmail.com>
2018-09-04 21:13:15 +0200

    Make UserRepository return empty collection when the result from IDataGateway is null

--- SnipCodeAPI/Repositories/UserRepository.cs
+++ SnipCodeAPI/Repositories/UserRepository.cs
@@ -12,7 +12,7 @@ namespace SnipCodeAPI.Repositories
     public UserRepository(IDataGateway DataGateway) => this.DataGateway = DataGateway;
     public void DeleteUser(int id) => DataGateway.RemoveUser(id);
     public User GetUserById(int userId) => DataGateway.GetUserByID(userId);
-    public IEnumerable<User> GetUsers() => DataGateway.GetAllUsers();
+    public IEnumerable<User> GetUsers() => DataGateway.GetAllUsers() ?? new List<User>();
     public void InsertUser(User user) => DataGateway.InsertUser(user);
     public void UpdateUser(User user) => DataGateway.UpdateUser(user);
   }
