412f07736fee1c3151a35ac55d7b9c08b4368f13
Jakub Kuniszewski <jakubkuniszewski1@gmail.com>
2018-09-04 17:56:40 +0200

    Add 2 basic Unit Tests for testubg UserRepository Reading methods

---
+++ SnipCodeAPITests/UserRepositoryTests.cs
@@ -0,0 +1,38 @@
+using NUnit.Framework;
+using NSubstitute;
+using SnipCodeAPI.Repositories;
+using SnipCodeAPI.Repositories.Interfaces;
+using SnipCodeAPI.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SnipCodeAPITests
+{
+    [TestFixture]
+    public class UserRepositoryTests
+    {
+      [Test]
+      public void GetUsers_WhenNoUsers_ReturnsNull()
+      {
+        IDataGateway gateway = Substitute.For<IDataGateway>();
+
+        UserRepository repository = new UserRepository(gateway);
+        var users = repository.GetUsers();
+
+        Assert.AreEqual(null,users);
+      }
+
+      [Test]
+      public void GetUsers_WhenUsersAreAvailable_ReturnsUsers()
+      {
+        IDataGateway gateway = Substitute.For<IDataGateway>();
+        gateway.GetAllUsers().Returns(new List<User>{
+          new User()
+        });
+
+        UserRepository repository = new UserRepository(gateway);
+        var users = repository.GetUsers();
+        Assert.AreEqual(1,users.Count());
+      }
+    }
+}
