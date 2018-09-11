178a9bb6e8bc26af6ae7dd642d10679e45d72408
Mikołaj Szymański <mikszym95@gmail.com>
2018-09-11 19:56:29 +0200

    Add CRUD test and change methods implementation

--- SnipCodeAPITests/UserRepositoryTests.cs
+++ SnipCodeAPITests/UserRepositoryTests.cs
@@ -5,34 +5,102 @@ using SnipCodeAPI.Repositories.Interfaces;
 using SnipCodeAPI.Models;
 using System.Collections.Generic;
 using System.Linq;
+using System;
 
 namespace SnipCodeAPITests
 {
     [TestFixture]
     public class UserRepositoryTests
     {
-      [Test]
-      public void GetUsers_WhenNoUsers_ReturnsNull()
-      {
-        IDataGateway gateway = Substitute.For<IDataGateway>();
+        #region Create
 
-        UserRepository repository = new UserRepository(gateway);
-        var users = repository.GetUsers();
+        [Test]
+        public void InsertUser_WhenUserToInsert_ShouldInsertOnce()
+        {
+            IDataGateway gateway = Substitute.For<IDataGateway>();
+            var repository = new UserRepository(gateway);
+            repository.InsertUser(new User());
+            gateway.Received(1).InsertUser(Arg.Any<User>());
+        }
 
-        Assert.AreEqual(null,users);
-      }
+        [Test]
+        public void InsertUser_WhenNoUserToInsert_ShouldNotInsert()
+        {
+            IDataGateway gateway = Substitute.For<IDataGateway>();
+            gateway.DidNotReceiveWithAnyArgs().InsertUser(Arg.Any<User>());
+        }
 
-      [Test]
-      public void GetUsers_WhenUsersAreAvailable_ReturnsUsers()
-      {
-        IDataGateway gateway = Substitute.For<IDataGateway>();
-        gateway.GetAllUsers().Returns(new List<User>{
+        #endregion
+
+        #region Read
+
+        [Test]
+        public void GetUsers_WhenNoUsers_ReturnsNull()
+        {
+            IDataGateway gateway = Substitute.For<IDataGateway>();
+
+            UserRepository repository = new UserRepository(gateway);
+            var users = repository.GetUsers();
+
+            Assert.AreEqual(null, users);
+        }
+
+        [Test]
+        public void GetUsers_WhenUsersAreAvailable_ReturnsUsers()
+        {
+            IDataGateway gateway = Substitute.For<IDataGateway>();
+            gateway.GetAllUsers().Returns(new List<User>
+            {
           new User()
-        });
+            });
 
-        UserRepository repository = new UserRepository(gateway);
-        var users = repository.GetUsers();
-        Assert.AreEqual(1,users.Count());
-      }
+            UserRepository repository = new UserRepository(gateway);
+            var users = repository.GetUsers();
+            Assert.AreEqual(1, users.Count());
+        }
+
+        #endregion
+
+        #region Update
+
+        [Test]
+        public void UpdateUser_WhenUserAreAvailable_ReturnsTrue()
+        {
+            IDataGateway gateway = Substitute.For<IDataGateway>();
+            gateway.GetAllUsers().Returns(new List<User>{
+            new User()
+          });
+            gateway.UpdateUser(Arg.Any<User>()).Returns(true);
+            var repository = new UserRepository(gateway);
+            Assert.IsTrue(repository.UpdateUser(new User()));
+        }
+
+        [Test]
+        public void UpdateUser_WhenUserAreNotAvailable_ReturnsFalse()
+        {
+            IDataGateway gateway = Substitute.For<IDataGateway>();
+            gateway.UpdateUser(Arg.Any<User>()).Returns(false);
+            var repository = new UserRepository(gateway);
+            Assert.IsFalse(repository.UpdateUser(new User()));
+        }
+
+        #endregion
+
+        #region Delete
+
+        [Test]
+        public void DeleteUser_WhenUserExists_ShouldDeleteUser()
+        {
+            IDataGateway gateway = Substitute.For<IDataGateway>();
+            gateway.GetAllUsers().Returns(new List<User>
+            {
+                new User(){Id = 1},
+                new User(){Id = 2}
+            });
+            var repository = new UserRepository(gateway);
+            repository.DeleteUser(2);
+            gateway.Received().RemoveUser(Arg.Any<int>());
+        }
+        #endregion
     }
 }
