7dc011e34fc6afda66cf5006f6d25a92ba6a9042
Jakub Kuniszewski <jakubkuniszewski1@gmail.com>
2018-11-26 16:56:41 +0100

    Add second unit test to the AuthServiceTests that checks if AuthService will return JWT when good credentials are passed

--- SnipCodeAPITests/AuthServiceTests.cs
+++ SnipCodeAPITests/AuthServiceTests.cs
@@ -34,7 +34,38 @@ namespace SnipCodeAPITests
       model.Email = "wrong@email.com";
       model.Password = "wrongpassword";
 
+      JsonWebToken result = authService.Authenticate(model);
+
       Assert.IsTrue(authService.Authenticate(model) == null);
+    }
+
+    [Test]
+    public void Authenticate_WhenExistingCredentialsGiven_ReturnsJsonWebToken()
+    {
+      IUserRepository userRepositoryMock = Substitute.For<IUserRepository>();
+
+      userRepositoryMock.GetUsers().Returns(new List<User>()
+      {
+        new User(){
+          Email = "Admin",
+          Password = "admin"
+        }
+      });
+
+      IPasswordHasher<User> passwordHasherMock = Substitute.For<IPasswordHasher<User>>();
+      ISeedService seedServiceMock = Substitute.For<ISeedService>();
+      IJWTService jwtServiceMock = Substitute.For<IJWTService>();
+      jwtServiceMock.Generate(Arg.Any<string>()).Returns(new JsonWebToken());
+      jwtServiceMock.RefreshTokens.Returns(new List<RefreshToken>());
+
+      AuthService authService = new AuthService(userRepositoryMock,passwordHasherMock,seedServiceMock,jwtServiceMock);
+      LoginViewModel model = new LoginViewModel();
+      model.Email = "Admin";
+      model.Password = "admin";
+
+      JsonWebToken result = authService.Authenticate(model);
+
+      Assert.IsTrue(result != null);
     }
   }
 }
