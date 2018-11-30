714d4751d11d82286405c23cdd01ded73431dc3f
Jakub Kuniszewski <jakubkuniszewski1@gmail.com>
2018-11-30 18:12:25 +0100

    Move method for decoding auth string to LoginViewModel class and update unit test for this method

--- SnipCodeAPITests/ExtensionTests.cs
+++ SnipCodeAPITests/ExtensionTests.cs
@@ -1,10 +1,9 @@
 using NUnit.Framework;
-using SnipCodeAPI.Extensions;
 using SnipCodeAPI.Models;
 
 namespace SnipCodeAPITests
 {
-  //Class with unit tests for extension methods
+  //Class with unit tests for extension & static methods
   [TestFixture]
   public class ExtensionTests
   {
@@ -12,7 +11,7 @@ namespace SnipCodeAPITests
     public void DecodeCredentials_ReturnsProperLoginViewModel_WhenValidBasicAuthStringIsGiven()
     {
       string testVal = "Basic QWRtaW46YWRtaW4=";
-      LoginViewModel result = testVal.DecodeCredentials();
+      LoginViewModel result = LoginViewModel.Decode(testVal);
       Assert.IsTrue(result.Email == "Admin" && result.Password == "admin");
     }
   }
