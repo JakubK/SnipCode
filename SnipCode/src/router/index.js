a02edbdac91865a2b703ae6c1b78089a6737a783
Jakub Kuniszewski <jakubkuniszewski1@gmail.com>
2018-12-28 19:06:04 +0100

    Remove HelloWorld component from frontend project

--- SnipCode/src/router/index.js
+++ SnipCode/src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 import SnippetEditor from '@/SnippetEditor'
 import AuthFrame from '@/AuthFrame'
 import Register from '@/Register'
@@ -19,8 +18,6 @@ export default new Router({
       children:[
         {
           path: '/',
-          name: "HelloWorld",
-          component: HelloWorld
         }
       ]
     },
