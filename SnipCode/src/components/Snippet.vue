80749be583875007908b22b88c91119747a2fdea
Jakub Kuniszewski <jakubkuniszewski1@gmail.com>
2018-12-28 19:18:39 +0100

    Fill Snippet component with simple structure and basic styling

--- SnipCode/src/components/Snippet.vue
+++ SnipCode/src/components/Snippet.vue
@@ -1,5 +1,13 @@
 <template>
   <main>
-    Snippet
+    <div class="snippet-panel">
+      <span>Snippet Name</span>
+      <button>New File</button>
+      <button>Share Snippet...</button>
+    </div>
+    <textarea></textarea>
   </main>
 </template>
+<style src="./Snippet.scss" lang="scss" scoped>
+
+</style>
