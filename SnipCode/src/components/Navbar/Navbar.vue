053a0233e59b4bc5aab5929caf6778f108fc0594
Jakub Kuniszewski <jakubkuniszewski1@gmail.com>
2019-01-12 16:07:01 +0100

    Change the view of Navbar and Snippet components by adding some svgs

--- SnipCode/src/components/Navbar/Navbar.vue
+++ SnipCode/src/components/Navbar/Navbar.vue
@@ -1,7 +1,9 @@
 <template>
   <nav>
-    <button>Logo</button>
-    <button>Snippet</button>
+    <a class="navbar-logo" href="#">SnipCode</a>
+    <button class="btn-new">
+      <svg aria-hidden="true" data-prefix="fas" data-icon="plus" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"></path></svg>
+      New Snippet</button>
   </nav>
 </template>
 <script>
