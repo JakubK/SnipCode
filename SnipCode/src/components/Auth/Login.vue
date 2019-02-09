6bf728132d0af2bce683da9370eeb4e9d55b34db
Jakub Kuniszewski <jakubkuniszewski1@gmail.com>
2019-02-09 16:47:13 +0100

    Add call to the Store for access token from credentials

--- SnipCode/src/components/Auth/Login.vue
+++ SnipCode/src/components/Auth/Login.vue
@@ -3,11 +3,36 @@
     <h2>Sign In</h2>
     <p>Log In to your SnipCode Account to manage your Snippets</p>
     <form class="login-form">
-      <input type="text" placeholder="Email Address"/>
-      <input type="password" placeholder="Password"/>
+      <input v-model="email" type="text" placeholder="Email Address"/>
+      <input v-model="password" type="password" placeholder="Password"/>
       <label>I forgot my Password</label>
-      <button>Sign in</button>
+      <button @click="TryToLogin">Sign in</button>
     </form>
   </div>
 </template>
 <style lang="scss" src="./Login.scss" scoped></style>
+<script>
+export default {
+  name: "Login",
+  data(){
+    return{
+      email: '',
+      password: ''
+    }
+  },
+  methods:
+  {
+    TryToLogin(e)
+    {
+      e.preventDefault();
+
+    
+        this.$store.dispatch("loginWithCredentials", {email: this.email, password: this.password}).then(() => {
+          //token acquired
+        })
+      
+
+    }
+  }
+}
+</script>
