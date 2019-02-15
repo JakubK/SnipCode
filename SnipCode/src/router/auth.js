df8c9e07d8a40f346287edb337a628d0615ccc56
Jakub Kuniszewski <jakubkuniszewski1@gmail.com>
2019-02-15 19:09:24 +0100

    Add special case to auth middleware for panel routes to check if token is valid

--- SnipCode/src/router/auth.js
+++ SnipCode/src/router/auth.js
@@ -2,7 +2,22 @@ import store from '../store/store';
 
 export default  (to, from, next) => {
   if (store.getters.token) {
-    next();
+     store.dispatch("validateToken").then(response => 
+      {
+        if(response.status === 200)
+        {
+          next();
+        }
+        else
+        {
+          store.dispatch("forgetToken");
+          next('/auth/register');
+        }
+      }).catch(() => 
+      {
+        store.dispatch("forgetToken");
+        next('/auth/register');
+      })
   } else {
     next('/auth/register');
   }
