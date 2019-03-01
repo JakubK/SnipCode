5f0220edd433c1626ce718f4567729a44dda5718
Jakub Kuniszewski <jakubkuniszewski1@gmail.com>
2019-03-01 18:23:48 +0100

    Create ChangePassword component for getting the input from user to ask server to change the password

---
+++ SnipCode/src/components/Profile/ChangePassword/ChangePassword.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="password-background">
+    <div class="password-panel">
+      <h1>Changing Password</h1>
+      <p>You're about to change your password</p>
+      <form>
+        <label>Old Password</label>
+        <input v-model="oldPassword"  type="password"/>
+        <br/>
+        <label>New Password</label>
+        <input v-model="newPassword"  type="password"/>
+        <br/>
+        <label>Repeat new Password</label>
+        <input v-model="newPasswordRepeat"  type="password"/>
+        <br/>
+        <button @click="handleSubmit" type="button">Change password</button>
+      </form>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'ChangePassword',
+  data()
+  {
+    return{
+      oldPassword: '',
+      newPassword: '',
+      newPasswordRepeat: ''
+    }
+  },
+  methods:
+  {
+    handleSubmit()
+    {
+       if(this.newPassword === this.newPasswordRepeat)
+       {
+        this.$store.dispatch("changePassword",{oldPassword: this.oldPassword,newPassword: this.newPassword}).then(result =>
+        {
+          this.$router.go(-1)
+        });
+       }
+    }
+  }
+}
+</script>
+<style lang="scss" src='./ChangePassword.scss' scoped></style>
