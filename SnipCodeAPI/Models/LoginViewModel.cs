b73f8fc6d4359416b1a5d38204cf3d4eb9f2e8af
Jakub Kuniszewski <jakubkuniszewski1@gmail.com>
2019-03-06 17:00:06 +0100

    Remove Decode static method of LoginViewModel with it's unit test file

--- SnipCodeAPI/Models/LoginViewModel.cs
+++ SnipCodeAPI/Models/LoginViewModel.cs
@@ -7,17 +7,5 @@ namespace SnipCodeAPI.Models
   {
     public string Email {get;set;}
     public string Password {get;set;}
-
-    public static LoginViewModel Decode(string authString)
-    {
-      var credValue = authString.Substring("Basic ".Length).Trim();
-      var usernameAndPassenc = Encoding.UTF8.GetString(Convert.FromBase64String(credValue)); //login:password
-      var usernameAndPass = usernameAndPassenc.Split(":");
-      
-      return new LoginViewModel{
-        Email = usernameAndPass[0],
-        Password = usernameAndPass[1]
-      };
-    }
   }
 }
